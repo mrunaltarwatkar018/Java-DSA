ce175da3787a2bd1fa758d4767cee9a9c661d6fb
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-05-06 00:45:30 +0530

    L 004 - Writing your first program

--- L 004 - Writing your first program/MyFirstProgram.java
+++ L 004 - Writing your first program/MyFirstProgram.java
@@ -1,22 +1,33 @@
-class MyFirstMG {
-    public static void main(String args[]){
+// class MyFirstMG {
+//     public static void main(String args[]){
+//         System.out.println("Hello Mrunal Tarwatkar 1");
+//         System.out.println(args[0]+ "sona --> GOLD");
+//         return;
+//     }
+//     public static void main1(String args[]){
+//         System.out.println("Hello Mrunal Tarwatkar 4");
+//     }
+// }
+
+// class MyFirstClass {
+//     public static void main(String args[]){
+//         System.out.println("Hello Mrunal Tarwatkar 2");
+//     }
+// }
+
+// class MySecondClass {
+//     public static void main(String args[]){
+//         System.out.println("Hello Mrunal Tarwatkar 3");
+//     }
+// }
+
+
+
+public class MyFirstProgram {
+
+    public static void main(String args[]) {
         System.out.println("Hello Mrunal Tarwatkar 1");
-        System.out.println(args[0]+ "sona --> GOLD");
+        System.out.println(args[0] + " sona --> GOLD");
         return;
-    }
-    public static void main1(String args[]){
-        System.out.println("Hello Mrunal Tarwatkar 4");
-    }
-}
-
-class MyFirstClass {
-    public static void main(String args[]){
-        System.out.println("Hello Mrunal Tarwatkar 2");
-    }
-}
-
-class MySecondClass {
-    public static void main(String args[]){
-        System.out.println("Hello Mrunal Tarwatkar 3");
     }
 }
---
+++ L 004 - Writing your first program/tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+class MyFirstMG {
+    public static void main(String args[]){
+        System.out.println("Hello Mrunal Tarwatkar 1");
+        System.out.println(args[0]+ "sona --> GOLD");
+        return;
+    }
+    public static void main1(String args[]){
+        System.out.println("Hello Mrunal Tarwatkar 4");
+    }
+}
